54a497f52e4326304e5e2c4a7bd2ea441d3dfd31
Jay <luvntruth77@gmail.com>
2018-06-21 06:03:40 -0400

    MIA Ironhack Lab - JS Memeory Game

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,33 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
 
-// };
+var MemoryGame = function (deckOfCards) {
+  this.cards = deckOfCards;
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+  this.currentPair = [];
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  var shuffledArray = [];
+  while (shuffledArray.length < 24) {
+    var randomNumber = Math.floor(Math.random() * this.cards.length)
+    var randomElement = this.cards[randomNumber];
+    shuffledArray.push(randomElement);
+    this.cards.splice(randomNumber, 1);
 
-// }
+  }
+  this.cards = shuffledArray;
+};
 
-// MemoryGame.prototype.finished = function () {
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++;
+  if (firstCard.attr('name') === secondCard.attr('name')) {
+    this.pairsGuessed++;
+  }
+  return false;
+}
 
-// };
+MemoryGame.prototype.finished = function () {
+  return this.pairsGuessed >= 12;
+};
+
